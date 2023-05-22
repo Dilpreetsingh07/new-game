6de1adaeccc8c9b364e7bc996a7c181c56632a7f
Dilpreetsingh07 <75797594+Dilpreetsingh07@users.noreply.github.com>
2023-05-22 19:53:06 +0530

    Add files via upload

---
+++ skecth.js
@@ -0,0 +1,21 @@
+var input;
+
+
+
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  
+  input=createSprite(windowWidth-100,windowHeight-100,200,200)
+  
+  
+}
+
+function draw() {
+  background(220);
+  
+  
+  
+  drawSprites();
+}
